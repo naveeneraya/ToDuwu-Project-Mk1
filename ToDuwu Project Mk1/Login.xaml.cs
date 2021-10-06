18e6ef72c4312289cc9300d5077a7176239006ba
Naveene Raya <48770741+naveeneraya@users.noreply.github.com>
2021-10-06 13:12:54 -0700

    added login, user info, and changed main window

--- ToDuwu Project Mk1/Login.xaml.cs
+++ ToDuwu Project Mk1/Login.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ToDuwu_Project_Mk1
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Login.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
         }
     }
 }
---
+++ ToDuwu Project Mk1/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDuwu_Project_Mk1
+{
+    // user information
+    // Naveene Raya
+    class UserInfo
+    {
+        public static String Name = "";
+        public static String Password = "";
+    }
+}
