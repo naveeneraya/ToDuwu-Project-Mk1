905ee7554d4c99b50d2b43aab4599731a69756ca
Kohvu <PJIPark@gmail.com>
2021-10-21 16:02:00 -0700

    ToDuwu Database
    Database added.  Task and User tables

---
+++ ToDuwu Database/dbo/Tables/Task.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Task]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserId] INT NOT NULL, 
+    [TaskName] NVARCHAR(50) NULL, 
+    [TaskDescription] NTEXT NULL, 
+    CONSTRAINT [FK_Task_ToUser] FOREIGN KEY (UserId) REFERENCES [User]([Id])
+)
