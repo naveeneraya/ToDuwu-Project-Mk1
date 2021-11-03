88d701db05a552ff741edb5221e797332c7a2f32
Naveene Raya <48770741+naveeneraya@users.noreply.github.com>
2021-11-03 12:56:38 -0700

    several changes fixed errors

--- ToDuwu Project Mk1/Task.xaml.cs
+++ ToDuwu Project Mk1/Task.xaml.cs
@@ -44,50 +44,7 @@ namespace ToDuwu_Project_Mk1
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            var datasource = @"Data Source=(localdb)\MSSQLLocalDB";   //your server
-            var database = "ToDuwu Database";   //your database name
 
-            //your connection string 
-            string connString = @"Data Source=" + datasource + ";Initial Catalog="
-                        + database + ";Persist Security Info=True;";
-
-
-            SqlConnection conn = null;
-
-            try
-            {
-
-                conn = new SqlConnection(connString);
-
-                //open connection
-                conn.Open();
-
-                //create a new SQL Query using StringBuilder
-                StringBuilder strBuilder = new StringBuilder();
-                strBuilder.Append(@"INSERT INTO [User] (Id, UserName, FirstName, LastName, HashedPW) ");
-                strBuilder.Append("VALUES (" + 1 + ", N'" + newUserTxt  + "', N'" + newFirstTxt + "', N'" + newlastTxt + "' , N'" + confirmPass + "'); ");
-
-                string sqlQuery = strBuilder.ToString();
-
-                using (SqlCommand command = new SqlCommand(sqlQuery, conn)) //pass SQL query created above and connection
-                {
-                    command.ExecuteNonQuery(); //execute the Query
-                    Console.WriteLine("Query Executed.");
-                }
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex.Message);
-            }
-
-            finally {
-                // makes sure conn is always closed at end
-                if (conn != null)
-                {
-                    conn.Close();
-                }
-            }
         }
     }
 }
