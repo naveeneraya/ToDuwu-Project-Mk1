7c8350de6cac29cb0679ac02730a03a354f1680d
Brenda Solis <brendab4solis@gmail.com>
2021-11-05 18:27:33 -0700

    Tasks updates

---
+++ ToDuwu Project Mk1/Task.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.SqlServer.Management.Smo.Wmi;
+using System.Collections.Generic;
+
+namespace ToDuwu_Project_Mk1
+{
+    /// <summary>
+    /// Interaction logic for Task.xaml
+    /// </summary>
+    public partial class Task : Window
+    {
+        private List<Task> taskList = new List<Task>();
+
+        public Task()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void ScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+     
+    }
+}
---
+++ ToDuwu Project Mk1/Tasks.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDuwu_Project_Mk1
+{
+   public  class Tasks: ICloneable
+    {
+        public static int ID=0;
+        public static String TaskName = "";
+        public static String TaskDescription = "";
+        public static DateTime dueDateTime = new DateTime();
+        public static Boolean delete = false;
+        public static Boolean complete = false;
+        public static String group = "";
+        public Tasks()
+        {
+            ID++;
+               }
+        public void addName(String x)
+        {
+            TaskName = x;
+        }
+        public void addDescription(String whatItIS)
+        {
+            TaskDescription = whatItIS;
+        }
+        public void setGroup(String g)
+        {
+            group = g;
+        }
+        public void setDueDate(DateTime y)
+        {
+            dueDateTime = y;
+        }
+        public void completeTask()
+        {
+            complete = true;
+        }
+        public void deleteTask()
+        {
+            delete = true;
+        }
+
+        public object Clone()
+        {
+            var task = (Task)MemberwiseClone();
+            return task;
+         }
+    }
+}
